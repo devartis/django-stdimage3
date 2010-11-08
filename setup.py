d17cdb8adb0f9ffc7f696197e4fb562d3f0fa8b9
haineault@gmail.com <haineault@gmail.com@98e0225f-c4bd-5141-cd17-7b7539b1d3f1>
2010-11-08 19:21:58 +0000

    Fixed stdimage issue #40

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='django-stdimage2',
+      version='0.1',
+      description='Django Standardized Image Field (fork)',
+      author='Maxime Haineault',
+      author_email='info@motion-m.ca',
+      url='http://code.google.com/p/django-stdimage2',
+      download_url='http://django-stdimage2.googlecode.com/svn/trunk',
+      packages=find_packages(),
+      license='GNU Lesser General Public License',
+      keywords='django image thumbnail',
+     )
+
